e85d87b48914d3597b8dd0bab2cb37e519c9fecf
toferx <mrsebipro@gmail.com>
2024-12-12 22:29:44 +0000

    Add files via upload

---
+++ tool.py
@@ -0,0 +1,82 @@
+
+
+
+import os
+
+
+
+
+
+## Functions
+
+def maintool():
+    print("""                                               
+,--------.       ,---.                         
+'--.  .--',---. /  .-' ,---. ,--.--.,--.  ,--. 
+   |  |  | .-. ||  `-,| .-. :|  .--' \  `'  /  
+   |  |  ' '-' '|  .-'\   --.|  |    /  /.  \  
+   `--'   `---' `--'   `----'`--'   '--'  '--' 
+                                               """)
+    print("")
+    print("Toferx is redefining all methords for yall yns")
+    print("")
+    print("1 = School Tools")
+    print("2 = Methords ( PREMIUM LOCKED )")
+    print("3 = Premium")
+    print("4 = Custom-made Console")
+    a=input("> ")
+    if a==4:
+        print("4")
+    if a==2:
+        print("2")
+    if a==1:
+        print("1")
+    if a==3:
+        print("3")
+
+def tos():
+    print("Terms of Service")
+    print("1.0 - 1.9 Responsibilitiys")
+    print("1.0 This document is a important document this will clarify everything that we are responsible.")
+    print("1.1 You must remember that you have responsibity about YOUR USAGE!")
+    print("1.2 Dont do dumb shit to land u in jail brother")
+    print("1.3 ( if in school ) Make sure to be following a IT SERVICE guideline.")
+    print("1.4 DO NOT skip this TOS, as it may help you in future.")
+    print("1.5 Make sure to have a updated version of mutitool. Its a responsibility.")
+    print("1.6 If you bought the tool from someone else PLEASE contact me.")
+    print("1.7 If you bought from others MALWARE RISK!")
+    print("1.8 People can disguse this tool as a virus! Be carefull")
+    print("1.9 During the making this is MALWARE FREE! People can mask malwares.")
+    input("Press enter once read")
+    os.system('cls')
+    print("Terms of Service")
+    print("2.0 - 2.9 User Agreement")
+    print("2.0 Usage of this tool means that you AGREE to all TOS.")
+    print("2.1 Usage of tool means that you AGREE that TOS can be changed anytime!")
+    print("2.2 Usage of tool means that any misuse is YOUR FAULT!")
+    print("2.3 We are not responsible for anything that is used with this tool.")
+    print("2.4 Education purposes ONLY.")
+    print("2.5 No legal actions are liable by us.")
+    print("More soon.")
+    input("Press enter once read")
+    os.system('cls')
+    maintool()
+
+
+
+## MAIN
+print("""                                               
+,--------.       ,---.                         
+'--.  .--',---. /  .-' ,---. ,--.--.,--.  ,--. 
+   |  |  | .-. ||  `-,| .-. :|  .--' \  `'  /  
+   |  |  ' '-' '|  .-'\   --.|  |    /  /.  \  
+   `--'   `---' `--'   `----'`--'   '--'  '--' 
+                                               """)
+
+print("")
+print("Toferx is redefining all methords for yall yns")
+print("")
+print("Enter To continue to READ **T*O*S**")
+input("Press enter to continue to TOS")
+os.system('cls')
+tos()
